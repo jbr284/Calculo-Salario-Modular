bf6a5607fcf302843c9806e47fd5f25199588de6
jbr284 <jbrosa2009@gmail.com>
2026-01-15 02:01:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'salario-Modular-cache-v27'; 
+const CACHE_NAME = 'salario-Modular-cache-v28'; 
 const urlsToCache = [
   './',
   'index.html',
@@ -67,6 +67,7 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
 
 
 
