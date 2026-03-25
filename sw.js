3d5c92d79636be14cafaf66be1bf21c1327d369d
jbr284 <jbrosa2009@gmail.com>
2026-03-25 18:06:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Aumentamos a versão para forçar a atualização dos novos arquivos
-const CACHE_NAME = 'salario-Modular-cache-v21'; 
+// Aumentamos a versão para forçar a atualização (Versão 22)
+const CACHE_NAME = 'salario-Modular-cache-v22'; 
 const urlsToCache = [
   './',
   'index.html',
